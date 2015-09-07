066ad2339fb141963c94d1d35fb58ed28e15aecb
Guchan Alkan <zehreken@gmail.com>
2015-09-07 17:55:03 +0300

    Update main.cpp

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -21,17 +21,31 @@ int main()
 {
 	Graph graph(10);
 	graph.addEdge(1, 2);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(1, 3);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(3, 4);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(2, 5);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(1, 6);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(2, 7);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(6, 8);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(8, 9);
+	graph.printAdjacencyMatrix();
 	graph.addEdge(8, 10);
-	
+	graph.printAdjacencyMatrix();
+
+	graph.printEdges();
+
 	cout << "node count: " << graph.getNodeCount() << endl;
 	cout << "edge count: " << graph.getEdgeCount() << endl;
-	
+
+	int a;
+	cin >> a;
+
 	return 0;
 }
