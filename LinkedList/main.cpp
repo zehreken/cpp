b0cd89f8cf46c6662c357eb110e3ff15c36d8dd4
Guchan Alkan <zehreken@gmail.com>
2015-09-16 00:41:14 +0300

    Simple implementation of a linked list.

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
+struct Node
+{
+	Node *next;
+	int data;
+};
+
+void printLinkedList(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << ", ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	Node *head = (Node*)malloc(sizeof(Node));
+	head->data = 0;
+	Node *temp = head;
+	for (int i = 1; i < 10; ++i)
+	{
+		Node *newNode = (Node*)malloc(sizeof(Node));
+		temp->next = newNode;
+		newNode->data = i;
+		temp = newNode;
+	}
+	
+	printLinkedList(head);
+	
 	return 0;
 }
