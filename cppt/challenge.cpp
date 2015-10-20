c170b1167c86f646456b8e52711e7ccfafcb2812
Guchan Alkan <zehreken@gmail.com>
2015-10-20 22:14:14 +0300

    Deque challenge/timeout

--- cppt/challenge.cpp
+++ cppt/challenge.cpp
@@ -1,66 +1,52 @@
-/*10 9
-2 1
-3 1
-4 3
-5 2
-6 1
-7 2
-8 6
-9 8
-10 8*/
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <deque> 
 using namespace std;
 
-class Node
-{
-	public:
-	int id, parent;
-	vector<Node*> firstLevelChildren;
-	
-	int getChildCount()
-	{
-		int childCount = firstLevelChildren.size();
-		for (int i = 0; i < firstLevelChildren.size(); ++i)
-		{
-			childCount += firstLevelChildren[i]->getChildCount();
-			// cout << id << " child count: " << firstLevelChildren[i]->firstLevelChildren.size() << endl;
-		}
-		
-		return childCount;
-	}
-};
-
 int main()
-{
-	int vCount, eCount;
-	scanf("%d %d", &vCount, &eCount);
-	
-	const int nodeCount = 10;
-	Node nodes[nodeCount];
-	for (int i = 0; i < nodeCount; ++i)
-	{
-		nodes[i].id = i + 1;
-	}
-	
-	for (int i = 0; i < eCount; ++i)
-	{
-		int ui, vi;
-		scanf("%d %d", &ui, &vi);
-		
-		nodes[ui - 1].parent = vi;
-		nodes[vi - 1].firstLevelChildren.push_back(&nodes[ui - 1]);
-	}
-	
-	printf("\n");
-	
-	for (int i = 0; i < vCount; ++i)
-	{
-		printf("id: %d parent: %d flc: %lu c: %d\n", nodes[i].id, nodes[i].parent, nodes[i].firstLevelChildren.size(), nodes[i].getChildCount());
-	}
-	
-	return 0;
+{ 
+    int testCount = 0;
+    int testCounter = 0;
+    scanf("%d", &testCount);
+    
+    while (testCounter < testCount)
+    {
+        int arraySize = 0;
+        int subArraySize = 0;
+        scanf("%d", &arraySize);
+        scanf("%d", &subArraySize);
+        deque<int> d;
+        for (int i = 0; i < arraySize; ++i)
+        {
+            int a = 0;
+            cin >> a;
+            d.push_back(a);
+            if (d.size() == subArraySize)
+            {
+                int max = 0;
+                for (int i = 0; i < subArraySize; ++i)
+                {
+                    if (d[i] > max) max = d[i];
+                }
+                printf("%d ", max);
+                d.pop_front();
+            }
+        }
+        /*
+        while (d.size() >= subArraySize)
+        {
+            int max = 0;
+            for (int i = 0; i < subArraySize; ++i)
+            {
+                if (d[i] > max) max = d[i];
+            }
+            d.pop_front();
+            printf("%d ", max);
+        }
+        //*/
+        printf("\n");
+        ++testCounter;
+    }
+    
+    return 0;
 }
+
