40b7f1033048b902335502d159f3134bff262bff
Guchan Alkan <zehreken@gmail.com>
2016-01-25 02:34:14 +0200

    Some cleaning up.

--- cppt/main.cpp
+++ cppt/main.cpp
@@ -3,46 +3,25 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <sstream>
 using namespace std;
-
-vector<int> calcBigInt()
-{
-    
-}
-
-// Tn+2 = (Tn+1)^2 + Tn
-int calcExpTerm(int firstTerm, int secondTerm, int expTerm)
-{
-    vector<int> bigInt(20000, 0);
-    bigInt[0] = 1;
-    int bigDigit = 0;
-    
-    int nTerm, nplusoneTerm, nplustwoTerm;
-    nTerm = firstTerm;
-    nplusoneTerm = secondTerm;
-    for (int i = 2; i < expTerm; ++i)
-    {
-        for (int j = 0; j < bigInt.size(); ++j)
-        {
-            int digit = bigInt[i];
-            // digit = digit
-        }
-        nplustwoTerm = pow(nplusoneTerm, 2) + nTerm;
-        nTerm = nplusoneTerm;
-        nplusoneTerm = nplustwoTerm;
-    }
-}
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    
-    int firstTerm, secondTerm, expTerm;
-    string input;
-    cin >> firstTerm;
-    cin >> secondTerm;
-    cin >> expTerm;    
-    cout << calcExpTerm(firstTerm, secondTerm, expTerm) << endl;
-    
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (j >= N - 1 -i)
+				cout << "#";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
     return 0;
 }
+
